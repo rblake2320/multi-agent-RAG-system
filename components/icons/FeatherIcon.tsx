98a0de28a72c768af6780fe4868b569f6d325766
UltraSecure Developer <developer@ultrasecure.email>
2025-10-03 17:27:36 -0500

    chore: health sweep - add CI/CD and formatting

--- components/icons/FeatherIcon.tsx
+++ components/icons/FeatherIcon.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
-export const FeatherIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
+export const FeatherIcon: React.FC<React.SVGProps<SVGSVGElement>> = props => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
     <path d="M20.24 12.24a6 6 0 0 0-8.49-8.49L5 10.5V19h8.5z" />
     <path d="m16 8-2-2" />
     <path d="M12 12H2" />
